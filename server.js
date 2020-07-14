02f98977c4786b4ecbf04c9fd83956d5dbae3890
EdwinArevalo <edwin.arevalo@tecsup.edu.pe>
2020-07-14 12:41:29 -0500

    finished arevalo

---
+++ server.js
@@ -0,0 +1,30 @@
+const express =require('express');
+const user = require('./models/user');
+const app = express();
+
+http = require('http').Server(app);
+io = require('socket.io')(http);
+
+app.set('port', process.env.PORT || 3000)
+.set('view engine','jade');
+
+app.use('/static', express.static('public'))
+.get('/', (req, res) => {
+    res.render('main');
+});
+ 
+io.on('connection', (socket)=> {
+    console.log('Usuario conectado');
+    socket.on('crear', (data)=> {
+        user.create(data, (rpta)=> {
+            io.emit('nuevo', rpta);
+        });
+    });
+    socket.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+});
+
+http.listen(app.get('port'), ()=> {
+    console.log('Server on port ', app.get('port'));
+});
